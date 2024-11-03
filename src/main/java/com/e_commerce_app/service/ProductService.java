bfacd7c4ea2f5451e010650f142947ed7d09ff2f
ajaygulia17 <ajaygkumar24@gmail.com>
2024-11-04 01:32:37 +0530

    Adds ProductService with methods for CRUD operations

---
+++ src/main/java/com/e_commerce_app/service/ProductService.java
@@ -0,0 +1,28 @@
+package com.e_commerce_app.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.e_commerce_app.entity.Product;
+import com.e_commerce_app.repository.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public List<Product> getAllProduct(){
+		return productRepository.findAll();
+	}
+	
+	public Product findProductById(Long id){
+		return productRepository.findById(id).orElseThrow();
+	}
+	
+	public Product saveProduct(Product product) {
+		return productRepository.save(product);
+	}
+}
