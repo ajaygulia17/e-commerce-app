07fb1ac894587f95567a7f934a3fc32773f33e6e
ajaygulia17 <ajaygkumar24@gmail.com>
2024-11-04 01:22:11 +0530

    Adds Product entity and repository

---
+++ src/main/java/com/e_commerce_app/entity/Product.java
@@ -0,0 +1,79 @@
+package com.e_commerce_app.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	@Column(nullable = false)
+	private String description;
+	@Column(nullable = false)
+	private BigDecimal price;
+	@Column(nullable = false)
+	private int stock;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public Product(long id, String name, String description, BigDecimal price, int stock) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.stock = stock;
+	}
+
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/com/e_commerce_app/repository/ProductRepository.java
@@ -0,0 +1,5 @@
+package com.e_commerce_app.repository;
+
+public interface ProductRepository {
+
+}
