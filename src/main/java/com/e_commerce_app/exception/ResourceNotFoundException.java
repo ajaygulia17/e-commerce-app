f93ccbecd7d29421224321b529251ce2157c9dab
ajaygulia17 <ajaygkumar24@gmail.com>
2024-11-06 23:46:30 +0530

    Adds Resource Not Found Exception

---
+++ src/main/java/com/e_commerce_app/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.e_commerce_app.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
