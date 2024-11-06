5657cc3ba265daceb0eed361bd6b9c5118db81ec
ajaygulia17 <ajaygkumar24@gmail.com>
2024-11-06 21:21:09 +0530

    Adds Product RestController for CRUD Operation

---
+++ src/main/java/com/e_commerce_app/controller/ProductController.java
@@ -0,0 +1,43 @@
+package com.e_commerce_app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.e_commerce_app.entity.Product;
+import com.e_commerce_app.service.ProductService;
+
+@RestController
+@RequestMapping("api/products")
+public class ProductController {
+
+	@Autowired
+	ProductService productService;
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> getAllProduct(){
+		List<Product> products = productService.getAllProduct();
+		return new ResponseEntity<>(products, HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+		Product product = productService.findProductById(id);
+		return new ResponseEntity<>(product, HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Product> addProduct(Product product) {
+		Product saved =  productService.saveProduct(product);
+		return new ResponseEntity<>(saved, HttpStatus.CREATED);
+	}
+	
+	
+}
