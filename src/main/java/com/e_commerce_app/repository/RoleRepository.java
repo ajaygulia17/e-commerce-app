f63859db56b97e75a75d90ed730283c397a0a12c
ajaygulia17 <ajaygkumar24@gmail.com>
2024-11-07 14:22:19 +0530

    Adds Role Entity and RoleRepository

---
+++ src/main/java/com/e_commerce_app/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.e_commerce_app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.e_commerce_app.entity.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
