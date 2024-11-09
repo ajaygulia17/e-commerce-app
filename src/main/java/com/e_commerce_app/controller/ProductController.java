15ca7ca6d8f7b5ce3b336a0517f4fd28196741ba
ajaygulia17 <ajaygkumar24@gmail.com>
2024-11-09 18:41:46 +0530

    Adds Method level authentication

--- src/main/java/com/e_commerce_app/controller/ProductController.java
+++ src/main/java/com/e_commerce_app/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,25 +23,29 @@ public class ProductController {
 
 	@Autowired
 	ProductService productService;
-	
+
+	@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 	@GetMapping
 	public ResponseEntity<List<Product>> getAllProduct(){
 		List<Product> products = productService.getAllProduct();
 		return new ResponseEntity<>(products, HttpStatus.OK);
 	}
-	
+
+	@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 	@GetMapping("/{id}")
 	public ResponseEntity<Product> getProductById(@PathVariable Long id) {
 		Product product = productService.findProductById(id);
 		return new ResponseEntity<>(product, HttpStatus.OK);
 	}
-	
+
+	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping
 	public ResponseEntity<Product> addProduct(@RequestBody Product product) {
 		Product saved =  productService.saveProduct(product);
 		return new ResponseEntity<>(saved, HttpStatus.CREATED);
 	}
-	
+
+	@PreAuthorize("hasRole('ADMIN')")
 	@DeleteMapping("{id}")
 	public ResponseEntity<Object> deleteProduct(@PathVariable Long id){
 		productService.deleteProduct(id);
--- src/main/java/com/e_commerce_app/repository/UserRepository.java
+++ src/main/java/com/e_commerce_app/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.e_commerce_app.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.e_commerce_app.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 	
-	public User findByUsername(String username);
+	Optional<User> findByUsername(String username);
 }
