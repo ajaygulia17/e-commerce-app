fd1c6287a75c1535ae39d9c511d6b1b0d65b84eb
ajaygulia17 <ajaygkumar24@gmail.com>
2024-11-21 14:51:31 +0530

    Add UserRegistrationDTO for collecting user registration data

---
+++ src/main/java/com/e_commerce_app/dto/UserRegistrationDTO.java
@@ -0,0 +1,60 @@
+package com.e_commerce_app.dto;
+
+public class UserRegistrationDTO {
+
+	private String name;
+	private String username;
+	private String password;
+	private String confirmPassword;
+	private String email;
+	private Long roleId = 1L;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(Long roleId) {
+		this.roleId = roleId;
+	}
+
+}
